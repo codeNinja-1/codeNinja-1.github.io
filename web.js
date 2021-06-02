8d0241d8bbb7ea43139f984160e728eb16229cf4
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2021-06-02 12:22:21 -0700

    Update web.js

--- web.js
+++ web.js
@@ -22,3 +22,27 @@ web.loadNavigationBar = function(navElement, section) {
     navElement.appendChild(link);
   }
 };
+
+web.loadIcon = function() {
+  var container = document.createElement("div");
+    container.style.display = "none";
+    document.body.appendChild(container);
+  var image = document.createElement("img");
+    image.src = "https://avatars.githubusercontent.com/u/74162951";
+    image.onload = function() {
+      var canvas = document.createElement("canvas");
+        var context = canvas.getContext("2d");
+          context.beginPath();
+          context.beginPath();
+          context.arc(16, 16, 16, 0, Math.PI * 2);
+          context.clip();
+          context.drawImage(image, 0, 0, 32, 32);
+          context.restore();
+      var link = document.createElement("link");
+        link.setAttribute("rel", "icon");
+        link.setAttribute("href", canvas.toDataURL());
+        document.head.appendChild(link);
+      document.body.removeChild(container);
+    };
+    container.appendChild(image);
+}
