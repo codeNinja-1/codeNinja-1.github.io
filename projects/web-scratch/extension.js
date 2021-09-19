5f21f42c402947afe29277c62be76f2078f22768
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2021-09-19 12:50:11 -0700

    Update extension.js

--- projects/web-scratch/extension.js
+++ projects/web-scratch/extension.js
@@ -1,28 +1,10 @@
-// We use class syntax to define our extension object
-// This isn't actually necessary, but it tends to look the best
-
 class Web {
-  /**
-   * Scratch will call this method *once* when the extension loads.
-   * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
-   */
   getInfo() {
     return {
-      // `id` is the internal ID of the extension
-      // It should never change!
-      // If you choose to make an actual extension, please change this to something else.
-      // Only the characters a-z and 0-9 can be used. No spaces or special characters.
       id: 'web',
-
-      // `name` is what the user sees in the toolbox
-      // It can be changed without breaking projects.
       name: 'Web',
-
       blocks: [
         {
-          // `opcode` is the internal ID of the block
-          // It should never change!
-          // It corresponds to the class method with the same name.
           opcode: 'ipAddress',
           blockType: Scratch.BlockType.REPORTER,
           text: 'ip address'
@@ -30,28 +12,29 @@ class Web {
       ]
     };
   }
-
-  /**
-   * Corresponds to `opcode: 'hello'` above
-   */
   ipAddress() {
-    // You can just return a value: any string, boolean, or number will work.
-    // If you have to perform an asynchronous action like a request, just return a Promise.
-    // The block will wait until the Promise resolves and return the resolved value.
     return new Promise(function (resolve, reject) {
-      // Make the request
       fetch('https://www.cloudflare.com/cdn-cgi/trace')
-      // Extract JSON body content from HTTP response
       .then(response => response.text())
-      // Do something with the JSON data
-      .then(data => {
+      .then(function(data) {
         console.log(data);
-        resolve("hello, world");
+        data = parseData(data);
+        console.log(data);
+        resolve(data.ip);
       });
     });
+    function parseData(data) {
+      let result = {};
+      for (var i = 0; i < data.split("\n").length; i++) {
+        let pair = data.split("\n")[i];
+        let index = pair.indexOf("=");
+        let key = pair.slice(0, index);
+        let value = pair.slice(index + 1, pair.length);
+        result[key] = value;
+      }
+      return result;
+    }
   }
 }
 
-// Call Scratch.extensions.register to register your extension
-// Make sure to register each extension exactly once
 Scratch.extensions.register(new Web());
