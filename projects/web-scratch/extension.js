c97a8d0d78116a8378a11b9a52c50531c40c1848
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2021-09-19 12:35:45 -0700

    Update extension.js

--- projects/web-scratch/extension.js
+++ projects/web-scratch/extension.js
@@ -42,12 +42,15 @@ class Web {
       fetch('https://www.cloudflare.com/cdn-cgi/trace')
       .then(function (response) {
         console.log(response);
+        console.log(response.text());
         return response.text();
       })
       .then(function (data) {
         resolve(data.trim().split('\n').reduce(function(obj, pair) {
           pair = pair.split('=');
-          return obj[pair[0]] = pair[1], obj;
+          var value = (obj[pair[0]] = pair[1], obj);
+          console.log(value);
+          return value;
         }, {}));
       });
     });
