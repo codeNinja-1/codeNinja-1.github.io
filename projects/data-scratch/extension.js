adfef0ed8257cb4db84c6adc9357d0229e958ff7
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2021-09-19 13:20:11 -0700

    Create extension.js

---
+++ projects/data-scratch/extension.js
@@ -0,0 +1,82 @@
+class Data {
+  getInfo() {
+    return {
+      id: 'data',
+      name: 'Data+',
+      blocks: [
+        {
+          opcode: 'itemOf',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'item [ONE] of [TWO]',
+          arguments: {
+            ONE:{
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: "1"
+            },
+            TWO:{
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "[\"apple\", \"banana\", \"carrot\"]"
+            }
+          }
+        },
+        {
+          opcode: 'value',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'value of [ONE]',
+          arguments: {
+            ONE:{
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "\"banana\""
+            }
+          }
+        },
+        {
+          opcode: 'json',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'number [ONE]',
+          arguments: {
+            ONE:{
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 0
+            }
+          }
+        },
+        {
+          opcode: 'json',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'boolean [ONE]',
+          arguments: {
+            ONE:{
+              type: Scratch.ArgumentType.BOOLEAN,
+              defaultValue: true
+            }
+          }
+        },
+        {
+          opcode: 'json',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'string [ONE]',
+          arguments: {
+            ONE:{
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "banana"
+            }
+          }
+        }
+      ]
+    };
+  }
+  itemOf(args) {
+    return JSON.stringify(JSON.parse(args.TWO)[args.ONE]);
+  }
+  json(args) {
+    return JSON.stringify(args.ONE);
+  }
+  value(args) {
+    var json = JSON.parse(args.ONE);
+    if (json instanceof Object) return args.ONE;
+    else return json;
+  }
+}
+
+Scratch.extensions.register(new Data());
